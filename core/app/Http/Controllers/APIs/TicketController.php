d86d3c0eb6f1c642559baf12ce7a57f06d5255b0
salmanprog <m.salmanprog@gmail.com>
2025-09-08 14:24:48 +0500

    debuging request

--- core/app/Http/Controllers/APIs/TicketController.php
+++ core/app/Http/Controllers/APIs/TicketController.php
@@ -28,6 +28,8 @@ class TicketController extends BaseAPIController
             return $this->sendResponse(400, 'Validation Error', $validator->errors());
         }
 
+        print_r($request->all());
+        die();
         $baseUrl = Helper::GeneralSiteSettings('external_api_link_en');
         $authCode = Helper::GeneralSiteSettings('auth_code_en');
         $date = Carbon::today()->toDateString();
