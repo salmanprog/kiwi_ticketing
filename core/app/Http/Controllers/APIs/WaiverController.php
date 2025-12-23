60f578b7a440e311a9bee916c9988553cac2541c
salmanprog <m.salmanprog@gmail.com>
2025-12-23 20:11:16 +0500

    change name tickets

--- core/app/Http/Controllers/APIs/WaiverController.php
+++ core/app/Http/Controllers/APIs/WaiverController.php
@@ -167,37 +167,30 @@ class WaiverController extends BaseAPIController
         $waiver = Waiver::with(['media_slider'])->where('id',$entriesWaiver->id)->where('status','1')->first();
 
         //Send Email
-        try {
-            $get_mail_content = Email::where('identifier', 'waiver_email_confirmation')
-            ->where('status', '1')
-            ->first();
-            $raw_content = $get_mail_content->content;
-            $placeholders = [
-                '{ERROR_INFORMATION}'    => $request->body_content ?? 'No Issue Found'
-            ];
-            $raw_content = preg_replace('/\{(?:<[^>]+>)*(\w+)(?:<\/[^>]+>)*\}/', '{$1}', $raw_content);
-            $parsed_content = str_replace(array_keys($placeholders), array_values($placeholders), $raw_content);
+        $get_mail_content = Email::where('identifier', 'waiver_email_confirmation')
+        ->where('status', '1')
+        ->first();
+        $raw_content = $get_mail_content->content;
+        $placeholders = [
+            '{ERROR_INFORMATION}'    => $request->body_content ?? 'No Issue Found'
+        ];
+        $raw_content = preg_replace('/\{(?:<[^>]+>)*(\w+)(?:<\/[^>]+>)*\}/', '{$1}', $raw_content);
+        $parsed_content = str_replace(array_keys($placeholders), array_values($placeholders), $raw_content);
 
-            $email_subject = $get_mail_content->subject;
-            $from_email = config('mail.from.address');
-            $from_name = 'BolderAdventurePark';
-            $get_emails = array_map('trim', explode(',', $get_mail_content->to_reciever));
-            foreach ($get_emails as $email) {
-                Mail::send('emails.template', [
-                    'title' => $email_subject,
-                    'details' => $parsed_content
-                ], function ($message) use ($email_subject, $email, $from_email, $from_name) {
-                    $message->from($from_email, $from_name);
-                    $message->to($email);
-                    $message->replyTo($from_email, $from_name);
-                    $message->subject($email_subject);
-                });
-            }
-
-            return $this->sendResponse(200, 'Email sent successfully.');
-
-        } catch (\Exception $e) {
-            return $this->sendResponse(500, 'Server Error', $e->getMessage());
+        $email_subject = $get_mail_content->subject;
+        $from_email = config('mail.from.address');
+        $from_name = 'BolderAdventurePark';
+        $get_emails = array_map('trim', explode(',', $get_mail_content->to_reciever));
+        foreach ($get_emails as $email) {
+            Mail::send('emails.template', [
+                'title' => $email_subject,
+                'details' => $parsed_content
+            ], function ($message) use ($email_subject, $email, $from_email, $from_name) {
+                $message->from($from_email, $from_name);
+                $message->to($email);
+                $message->replyTo($from_email, $from_name);
+                $message->subject($email_subject);
+            });
         }
 
         $resource = WaiverResource::make($waiver);
