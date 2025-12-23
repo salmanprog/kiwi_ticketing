830784970eeabcf6dbc20f56588dd8e19a594f6a
salmanprog <m.salmanprog@gmail.com>
2025-12-24 01:57:43 +0500

    change name tickets

--- core/app/Helpers/MailHelper.php
+++ core/app/Helpers/MailHelper.php
@@ -25,40 +25,94 @@ class MailHelper
             $raw_content = $get_mail_content->content;
             $package = in_array($data->type, $allowedTypes) ? $data->{$data->type} : '';
             $ticketsHtml = '
-                <table border="1" width="100%" cellspacing="0" cellpadding="0" bordercolor="#cbcbcb" class="_table_with_border">
+                <table border="1" width="100%" cellspacing="0" cellpadding="0" bordercolor="#cbcbcb">
                     <tr>
-                        <td style="padding:5px">Ticket</td>
+                        <td style="padding:5px">Product(s)</td>
+                        <td style="padding:5px">Quantity</td>
                         <td style="padding:5px">Price</td>
-                        <td style="padding:5px">Scan</td>
                     </tr>';
 
                 foreach ($data->purchases as $purchase) {
                     $ticketsHtml .= '
                     <tr>
                         <td style="padding:5px">' . ($purchase->ticketType ?? $purchase->description) . '</td>
+                        <td style="padding:5px">$' . $purchase->quantity . '</td>
                         <td style="padding:5px">$' . number_format($purchase->price, 2) . '</td>
-                        <td style="padding:5px"> <img src="https://quickchart.io/qr?text='.$purchase->visualId.'&margin=2&size=100"
-                                                        title="Bolder Ticket QR Code" class="cabana_qr_code" /></td>
                     </tr>';
                 }
+                $ticketsHtml .= '
+                        <tr>
+                            <td colspan="2" style="padding:5px">
+                                <strong>Tax 8.25%</strong>
+                            </td>
+                            <td style="padding:5px">
+                                $'.$data->tax.'
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="padding:5px">
+                                <strong>Service Fee 2.75%</strong>
+                            </td>
+                            <td style="padding:5px">
+                                $'.$data->serviceCharges.'
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="padding:5px">
+                                <strong>Total:</strong>
+                            </td>
+                            <td style="padding:5px">
+                                '.$data->orderTotal.'
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding:5px" colspan="3">
+                                <h4>See / Modify Tickets</h4>
+                                <p style="padding:0; margin:0">To modify the date of your visit, please use the link
+                                    provided on your emailed receipt. Must change more than 24 hours in advance. tickets
+                                    purchased within 24 hours will not be able to be rescheduled.</p>
+                                <a href="https://tickets.bolderadventurepark.com/update-order/?order='.$data->slug.'" style="margin-bottom: 10px;">Click here to Modify</a>
+                                <ul class="_ticket_code" style="text-align:center;padding: 0;list-style: none;">';
+                                 foreach ($data->purchases as $purchase) {
+                                    $ticketsHtml .= '
+                                    <li style="margin: 0; border-bottom: 1px solid #b2b2b2; padding-bottom: 5px;">
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <h5>Ticket
+                                            - '.$purchase->ticketType.'
+                                        </h5>
+                                        <img src="https://quickchart.io/qr?text='.$purchase->visualId.'&margin=2&size=100"
+                                                        title="Bolder Ticket QR Code" class="cabana_qr_code" />
+                                        <p style="margin: 0;">'.$purchase->visualId.' </p>
+                                        
+                                            '.$purchase->ticketType.' - '.$purchase->slotTime.'
+
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                    </li>';
+                                }   
+                                '</ul>
+                            </td>
+                        </tr>
+                ';
             $ticketsHtml .= '</table>';
             $customerHtml = '
-                <table border="1" width="100%" cellspacing="0" cellpadding="0" bordercolor="#cbcbcb" class="_table_with_border">
-                    <tr>
-                        <td style="padding:5px">Name</td>
-                        <td style="padding:5px">Email</td>
-                        <td style="padding:5px">Phone</td>
-                        <td style="padding:5px">Address</td>
-                    </tr>
-                    <tr>
-                        <td style="padding:5px">' . $data->customer->name . '</td>
-                        <td style="padding:5px">' . $data->customer->email . '</td>
-                        <td style="padding:5px">' . $data->customer->phone . '</td>
-                        <td style="padding:5px">' . $data->customerAddress . '</td>
-                    </tr>';
+                <table border="0" style="border: 1px solid #ececec;" width="100%">
+                <h4>Customer Information</h4>
+                    <p>
+                        ' . $data->customer->name . ' <br />
+                        ' . $data->customer->email . ' <br />
+                        ' . $data->customer->phone . ' <br />
+                        ' . $data->customerAddress . '<br />
+                    </p>';
             $customerHtml .= '</table>';
             $billingHtml = '
-                <table border="1" width="100%" cellspacing="0" cellpadding="0" bordercolor="#cbcbcb" class="_table_with_border">
+                <table border="0" style="border: 1px solid #ececec;" width="100%">
+                <h4>Billing Information</h4>
                     <tr>
                         <td style="padding:5px">Gatway</td>
                         <td style="padding:5px">Transaction Id</td>
@@ -71,7 +125,25 @@ class MailHelper
                         <td style="padding:5px">' . $data->transaction->transactionDate . '</td>
                         <td style="padding:5px">$'.number_format($data->transaction->amount, 2).'</td>
                     </tr>';
-            $billingHtml .= '</table>';
+            $billingHtml .= '</table><table border="0" style="border: 1px solid #ececec;" width="100%">
+                                <tr>
+                                    <td>
+                                        <h3>Terms & Conditions</h3>
+                                        <p style="font-size: 10px;">
+                                            1. No Refunds. You fully ASSUME ALL RISK whatsoever in relation to your park activities and experience. By entering Park, you automatically and fully waive any and all claims of any nature whatsoever, including without limitation for personal injuries, inadequate security or warnings, theft/damage of property, weather-related claims, acts of 3rd parties, and illnesses (air, food, water-borne etc…)
+                                        </p>
+                                        <p style="font-size: 10px;">
+                                            2. You (including anyone under your care/supervision) must comply with all safety rules, warning signs and instructions. Parents/Guardians are solely responsible for supervision of minors or others needing special care. No resale, ”rain checks”, exchanges or refunds (including for loss, theft or expired date). SAFETY 1st! Act responsibly.
+                                        </p>
+                                        <p style="font-size: 10px;">
+                                            3. Days/hours and ride/attraction availability are subject to change without notice. Certain rides, shows, attractions and special events, may require an additional charge or be subject to heigh, weight, or other restrictions at Park’s discretion. Park may deny admission or expel anyone, without refund, for: illegality, aggressive/disorderly conduct or threats, Park rule violation, drugs/alcohol (including marijuana), offensive conduct (e.g., inappropriate or inadequate clothing, markings/tattoos, gestures, language, etc…), possession of a firearm or any weapon-like device, line-cutting, stealing, property damage, or anything else which the park deems a threat to the welfare/safety of people, wildlife/animals or Park property.
+                                        </p>
+                                        <p style="font-size: 10px;">
+                                            4. This ticket is valid in 2025 only.
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>';
             $placeholders = [
                 '{USER_NAME}'    => $data->customer->name ?? 'Customer',
                 '{ORDER_NUMBER}' => $data->slug ?? '',
