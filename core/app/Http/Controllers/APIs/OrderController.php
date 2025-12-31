71e138f35efff764eac7b8c0e278f6c4013d9774
salmanprog <m.salmanprog@gmail.com>
2025-12-31 16:56:14 +0500

    Regenrate order

--- core/app/Http/Controllers/APIs/OrderController.php
+++ core/app/Http/Controllers/APIs/OrderController.php
@@ -600,7 +600,7 @@ class OrderController extends BaseAPIController
                     $get_previous_order->user_id  = $get_previous_order->user_id;
                     $get_previous_order->save();
 
-                    print_r($data);
+                    print_r($data['tickets']);
                     die();
                     if (isset($data['data']['tickets']) && is_array($data['data']['tickets'])) {
                         $tickets = mapTicketNamesFromApi($data['data']['tickets']);
